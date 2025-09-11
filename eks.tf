be731ead85ae725ae081ef84e41b48e4131b0f59
learing git <87185729+kalaikris@users.noreply.github.com>
2025-09-11 20:44:42 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,29 +1,25 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.5"
+  version = "~> 21.0"
 
-  cluster_name    = var.cluster_name
-  cluster_version = "1.32"
+  name    = var.cluster_name
+  kubernetes_version = "1.32"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  create_cluster_security_group = false
-  create_node_security_group    = false
-  cluster_security_group_id     = aws_security_group.eks_cluster_sg.id
-  node_security_group_id        = aws_security_group.eks_node_sg.id
 
-  cluster_enabled_log_types = [
-    "api",
-    "audit",
-    "authenticator",
-    "controllerManager",
-    "scheduler"
-  ]
+#  enabled_cluster_log_types = [
+#    "api",
+#    "audit",
+#    "authenticator",
+#    "controllerManager",
+#    "scheduler"
+#  ]
 
-  cluster_endpoint_public_access  = true
-  cluster_endpoint_private_access = true
-  cluster_endpoint_public_access_cidrs = ["YOUR_IP/32"]
+  endpoint_public_access  = true
+  endpoint_private_access = true
+  enable_cluster_creator_admin_permissions = true
 
   eks_managed_node_groups = {
     default = {
@@ -32,7 +28,7 @@ module "eks" {
       desired_size = 2
 
       instance_types = ["t3.medium"]
-      ami_type       = "AL2_x86_64"
+      ami_type       = "AL2023_x86_64_STANDARD"
       disk_size      = 20
 
       update_config = {
@@ -41,52 +37,44 @@ module "eks" {
     }
   }
 
-  # IAM user access
-  access_entries = [
-    {
-      name               = "eks-admin-access"
-      principal_arn      = var.iam_user_arn
-      policy_associations = [
-        {
-          policy_arn   = var.eks_policy_arn
-          access_scope = "cluster"
-        }
-      ]
-    }
-  ]
 }
 
 # kube-proxy
 resource "aws_eks_addon" "kube_proxy" {
   cluster_name    = module.eks.cluster_name
   addon_name      = "kube-proxy"
-  resolve_conflicts = "OVERWRITE"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
 }
 
 # CoreDNS
 resource "aws_eks_addon" "coredns" {
   cluster_name    = module.eks.cluster_name
   addon_name      = "coredns"
-  resolve_conflicts = "OVERWRITE"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
 }
 
 # VPC CNI
 resource "aws_eks_addon" "vpc_cni" {
   cluster_name    = module.eks.cluster_name
   addon_name      = "vpc-cni"
-  resolve_conflicts = "OVERWRITE"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
 }
 
 # Pod identity agent (optional)
 resource "aws_eks_addon" "pod_identity_agent" {
   cluster_name    = module.eks.cluster_name
   addon_name      = "eks-pod-identity-agent"
-  resolve_conflicts = "OVERWRITE"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
 }
 
 # Node monitoring agent (optional)
 resource "aws_eks_addon" "node_monitoring_agent" {
   cluster_name    = module.eks.cluster_name
   addon_name      = "eks-node-monitoring-agent"
-  resolve_conflicts = "OVERWRITE"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
 }
