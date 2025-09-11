c687d4c78c3c08dafea81ec7b569741fa105b323
learing git <87185729+kalaikris@users.noreply.github.com>
2025-09-11 17:52:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,4 +3,8 @@ variable "cluster_name" {
   type        = string
   default     = "my-eks-cluster"
 }
-
+variable "iam_user_arn" {
+  description = "The ARN of the IAM user to access the EKS cluster"
+  type        = string
+  default     = "ENTER THE IAM USER ARN"
+}
